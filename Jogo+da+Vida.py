a1278600a82c590b13c07be324450cdead78045e
Lécio Bourbon <lbourbon@gmail.com>
2017-06-23 13:24:22 -0300

    .

---
+++ Jogo+da+Vida.py
@@ -0,0 +1,80 @@
+
+# coding: utf-8
+
+# In[1]:
+
+'''JOGO DA VIDA ('John Conway´s Game of Life' por Lécio Bourbon)  para Jupyter Notebook
+É um automato que tem por objetivo provar que a regras simples podem dar origem a mecanismos complexos'''
+
+from time import sleep
+from copy import deepcopy
+from IPython.display import clear_output
+
+# -> MODIFIQUE O GRID ABAIXO COMO QUISER
+# -> 1 SIGNIFICA CÉLULA VIVA
+# -> 2 SIGNIFICA CÉLULA MORTA
+
+grid = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+
+# In[ ]:
+
+def conta_vizinhos(grid, x, y):
+    return sum(grid[x-1][y-1:y+2]) + sum(grid[x][y-1:y+2]) + sum(grid[x+1][y-1:y+2]) - grid[x][y]
+
+def aplica_regras(grid):
+    '''REGRAS: Qualquer célula viva com menos de dois vizinhos vivos morre de solidão
+Qualquer célula viva com mais de três vizinhos vivos morre de superpopulação.
+Qualquer célula morta com exatamente três vizinhos vivos se torna uma célula viva.
+Qualquer célula viva com dois ou três vizinhos vivos continua no mesmo estado para a próxima geração.'''
+    temp = deepcopy(grid)
+    for i in range(1, len(grid)-1):
+        for j in range(1, len(grid[0])-1):
+            
+            if grid[i][j] == 1 and (conta_vizinhos(grid, i, j) < 2 or conta_vizinhos(grid, i, j) > 3):
+                temp[i][j] = 0
+                
+            elif conta_vizinhos(grid, i, j) == 3 or (grid[i][j] == 1 and conta_vizinhos(grid, i, j) == 2):
+                temp[i][j] = 1
+                
+    return temp
+
+
+# In[ ]:
+
+gerações = int(input('Escolha o número de gerações. (ex: 100) '))
+velocidade = int(input('Escolha a velocidade, de 1 a 10:  '))
+
+for i in range(int(gerações/2)):
+    temp = aplica_regras(grid)
+    for linha in temp:
+        print(linha)
+    sleep(1/velocidade)
+    clear_output(wait=True)
+    grid = aplica_regras(temp)
+    for linha in grid:
+        print(linha)
+    sleep(1/velocidade)
+    clear_output(wait=True)
+
